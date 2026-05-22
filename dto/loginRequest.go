d540dda908158886a03e7c4100b12b7f22d03813
amrshaban2005 <amr_shaban2005@yahoo.com>
2026-05-23 03:24:35 +0400

    create lib

--- dto/loginRequest.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-
-type LoginRequest struct{
-	UserName string `json:"user_name"`
-	Password string `json:"password"`
-}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/amrshaban2005/banking-auth/app"
-)
-
-func main() {
-	app.Start()
-}
